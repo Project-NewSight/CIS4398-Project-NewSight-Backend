859fedc9a462053bd3bacc981d663444dea84701
Victor Awoleye <tup40950@temple.edu>
2025-10-21 10:20:15 -0400

    Basic Route Working

--- emergency_contact_backend/app/main.py
+++ emergency_contact_backend/app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Form,UploadFile, File
+from fastapi.responses import JSONResponse
 
 app = FastAPI(title="Emergency Contact API", version="1.0")
 
@@ -8,5 +9,19 @@ def root():
 
 @app.post("/emergency/alert")
 async def emergency_alert(
+user_id: str = Form(...),
+latitude: float = Form(...),
+longitude: float = Form(...),
+image: UploadFile = File(None)
+):
+    
+    google_maps_url = f"https://maps.google.com/?q={latitude},{longitude}"
 
-)
+    response = {
+    "status":"recieved",
+    "user_id": user_id,
+    "location": google_maps_url,
+    "image_filename": image.filename if image else "No Image Uploaded"
+        }
+
+    return JSONResponse(content=response)
