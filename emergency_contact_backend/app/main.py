a9fcd8721e9fb6d2aa8199212a49b8b8c30981bd
Victor Awoleye <tup40950@temple.edu>
2025-10-21 10:44:17 -0400

    Temporary Photo Storage Buffer

--- emergency_contact_backend/app/main.py
+++ emergency_contact_backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI,Form,UploadFile, File
 from fastapi.responses import JSONResponse
 
@@ -16,11 +17,19 @@ image: UploadFile = File(None)
 ):
     
     google_maps_url = f"https://maps.google.com/?q={latitude},{longitude}"
+    saved_filename = None
+
+    if image:
+        os.makedirs("uploads",exist_ok=True)
+        saved_filename = os.path.join("uploads", image.filename)
+        with open(saved_filename, "wb") as buffer:
+            buffer.write(await image.read())
 
     response = {
     "status":"recieved",
     "user_id": user_id,
     "location": google_maps_url,
+    "image_saved": bool(image),
     "image_filename": image.filename if image else "No Image Uploaded"
         }
 
