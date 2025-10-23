c6cc56a99e2215d23da6cfbf2c4637460145609f
Victor Awoleye <tup40950@temple.edu>
2025-10-23 15:47:51 -0400

    SMS messageing working

--- emergency_contact_backend/app/services/sms_service.py
+++ emergency_contact_backend/app/services/sms_service.py
@@ -1,2 +1,31 @@
+import vonage
+from dotenv import load_dotenv
 import os
-from twilio.rest import Client 
+
+load_dotenv()
+
+VONAGE_API_KEY = os.getenv("VONAGE_API_KEY")
+VONAGE_API_SECRET = os.getenv("VONAGE_API_SECRET")
+
+def send_sms(to_number,message):
+   auth = vonage.Auth(api_key=VONAGE_API_KEY, api_secret=VONAGE_API_SECRET)
+   client = vonage.Vonage(auth)
+   
+   response = client.sms.send({
+       "from_":"+16033338616", 
+       "to": to_number,
+       "text": message
+       })
+   
+   for msg in response.messages:
+      if msg.status == "0":
+         print("Message sent sucessfully")
+      else:
+         print("Failed to send message")
+     
+
+
+send_sms("+4012888347", "The Generator VJ Edgecomb is the goat")
+
+
+
